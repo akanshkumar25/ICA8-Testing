ff8b782b40e623576ad584df4934650c74af627a
Akansh <akuma352@asu.edu>
2022-10-27 15:24:51 -0700

    Added goodString test cases

--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -15,5 +15,17 @@ public class urinalsTest
         urinals testUrinals = new urinals();
         boolean result = testUrinals.goodString("test1");
         Assertions.assertFalse( result );
+        result = testUrinals.goodString("101010101010101");
+        Assertions.assertTrue(result);
+        result = testUrinals.goodString("102301");
+        Assertions.assertFalse(result);
+        result = testUrinals.goodString("000000");
+        Assertions.assertTrue(result);
+        result = testUrinals.goodString("111111111");
+        Assertions.assertTrue(result);
+        result = testUrinals.goodString("-1");
+        Assertions.assertTrue(result);
+        result = testUrinals.goodString("111111111111110000000000000");
+        Assertions.assertFalse(result);
     }
 }
